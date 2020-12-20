20c17ce60bdb7a1102ab9ca88faa7d9ea759b1a8
Sunil Tamang <technicalnepal31@gmail.com>
2020-12-20 22:07:15 +0545

    working with variables in java

---
+++ src/com/suniltamang/Main.java
@@ -0,0 +1,12 @@
+package com.suniltamang;
+
+public class Main {
+	public static void main(String[] args) {
+		byte age=21;
+		long viewCount=3_123_456_789L;
+		float price=10.99F;
+		System.out.println(age);
+		
+	}
+ 
+} 
