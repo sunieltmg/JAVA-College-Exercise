df2f7d5ab35e2a7edd5d1880fcadd4fd32419e46
Sunil Tamang <technicalnepal31@gmail.com>
2020-12-23 15:15:44 +0545

    some minor changes made

--- src/com/suniltamang/Main.java
+++ src/com/suniltamang/Main.java
@@ -1,5 +1,6 @@
 package com.suniltamang;
 import java.awt.Point;
+import java.util.Arrays;
 import java.util.Date;
 public class Main {
 	public static void main(String[] args) {
@@ -20,6 +21,25 @@ public class Main {
 		System.out.println(message.replace("Hello", "Nepal"));
 		System.out.println(message.toUpperCase());
 		System.out.println(message.toLowerCase());
+//		int[] numbers=new int[5];
+//		numbers[0]=1;
+//		numbers[1]=11;
+//		numbers[2]=111;
+//		numbers[3]=1111;
+//		numbers[4]=11111;
+//		System.out.println(Arrays.toString(numbers));
+//		
+		
+		int[] numbers= {1,11,2,1111,5};
+		Arrays.sort(numbers);
+		System.out.println(Arrays.toString(numbers));
+		
+		final float pi=3.14F;
+		System.out.println(3+4);
+		
+		double prices = (double)3/(double)4;
+		System.out.println(prices);
+		
 		
 	}
  
